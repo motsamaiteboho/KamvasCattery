2a173a61c56ef5448c148f7d90cc6925ed606eb5
motsamaiteboho <96079592+motsamaiteboho@users.noreply.github.com>
2022-04-22 14:56:58 +0200

    I have added bookings tests for the booking function

--- bookings_cost/test/bookings.tests.js
+++ bookings_cost/test/bookings.tests.js
@@ -1,1 +1,18 @@
+describe('Weekly booking', function () {
 
+    it("should return the normal booking costs for weekly days exept Wednesday and Friday", function () {
+
+        assert.equal(135.00, bookingCost({ check_in_day: 'Monday', duration: 3, cat_name : 'Snowy'}));
+
+    });
+    it("should return the 25% discounted booking amount from the normal booking costs for Wednesday's bookings", function () {
+
+        assert.equal(101.25, bookingCost({ check_in_day: 'Wednesday', duration: 3, cat_name : 'Snowy'}));
+
+    });
+    it("should return the 15% discounted booking amount from the normal booking costs for Friday's bookings", function () {
+
+        assert.equal(114.75, bookingCost({ check_in_day: 'Friday', duration: 3, cat_name : 'Snowy'}));
+
+    });
+});
