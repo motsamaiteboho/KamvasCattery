ae339799c5dc75e0d9d7518bf1b86ffa1e25a253
motsamaiteboho <96079592+motsamaiteboho@users.noreply.github.com>
2022-04-22 14:28:59 +0200

    created a faction to calculate the weekly wages

--- weekly_wages/weekly_wages.js
+++ weekly_wages/weekly_wages.js
@@ -1,3 +1,48 @@
 function weeklyWage(band, weeklyHours) {
+    var hourlyRate = 0;
+    switch (band) {
+        case 'A':
+            var hours = weeklyHours.split(',');
+            let bandAtotal = 0;
+            if (hours.length === 7) {
+                for (let i = 0; i < hours.length; i++) {
+                    bandAtotal += (hours[i] * 23)
+                }
+                hourlyRate = bandAtotal;
+            }
+            else {
+                hourlyRate = "missing day"
+            }
+            break;
+        case 'B':
+            var hours = weeklyHours.split(',');
+            let bandBtotal = 0;
+            if (hours.length === 7) {
+                for (let i = 0; i < hours.length; i++) {
+                    bandBtotal += (hours[i] * 37)
+                }
+                hourlyRate = bandBtotal;
+            }
+            else {
+                hourlyRate = "missing day"
+            }
+            break;
+        case 'C':
+            var hours = weeklyHours.split(',');
+            let bandCtotal = 0;
+            if (hours.length === 7) {
+                for (let i = 0; i < hours.length; i++) {
+                    bandCtotal += (hours[i] * 51)
+                }
+                hourlyRate = bandCtotal;
+            }
+            else {
+                hourlyRate = "missing day"
+            }
+            break;
+        default:
+            hourlyRate =0;
 
+    }
+    return hourlyRate;
 };
